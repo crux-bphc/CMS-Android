5c86f2bd20163c79d5667104c8c928835e8b6402
harsu-ag <harsu.ag@gmail.com>
2017-10-20 13:34:01 +0530

    Shifting to FirebaseJobDispatcher

--- app/src/main/java/helper/MyNotificationManager.java
+++ app/src/main/java/helper/MyNotificationManager.java
@@ -1,11 +1,7 @@
 package helper;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 
-import app.Constants;
 import crux.bphc.cms.service.NotificationService;
 
 /**
@@ -15,12 +11,13 @@ import crux.bphc.cms.service.NotificationService;
 public class MyNotificationManager {
     public static void startNotificationServices(Context context) {
         UserAccount userAccount = new UserAccount(context);
-        Intent intent = new Intent(context, NotificationService.class);
+        /*Intent intent = new Intent(context, NotificationService.class);
         PendingIntent pintent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarm.cancel(pintent);
+        alarm.cancel(pintent);*/
         if (userAccount.isLoggedIn() && userAccount.isNotificationsEnabled()) {
-            alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, Constants.TRIGGER_AT, Constants.INTERVAL, pintent);
+//            alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, Constants.TRIGGER_AT, Constants.INTERVAL, pintent);
+            NotificationService.startService(context);
         }
 
     }
