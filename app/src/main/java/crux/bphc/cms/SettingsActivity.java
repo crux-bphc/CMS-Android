2b8d5a1741716fb16921ef222a78f4b1b4c1268e
Abhijeet Viswa <abhijeetviswa@gmail.com>
2019-08-15 03:43:51 +0530

    Back button from SettingsActivity applies theme
    After the theme has been changed from the settings menu and the software
    back button is pressed, the theme wouldn't update properly.
    
    The theme is applied while an activity is created. However, the software back
    button doesn't recreate the previous activity, it just resumes it. This commit
    makes sure that the activity is re-created only if required. If there is no
    theme change, the previous activity will be resumed.
    
    Sequence of creation of the NotificationService has been modified to allow for
    early exit if need be.
    
    Close: #111

--- app/src/main/java/crux/bphc/cms/SettingsActivity.java
+++ app/src/main/java/crux/bphc/cms/SettingsActivity.java
@@ -2,9 +2,12 @@ package crux.bphc.cms;
 
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CompoundButton;
 
 import app.MyApplication;
@@ -18,12 +21,16 @@ public class SettingsActivity extends AppCompatActivity {
     @BindView(R.id.darkModeSwitch)
     SwitchCompat darkModeSwitch;
 
+    boolean themeChanged;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         if (MyApplication.getInstance().isDarkModeEnabled()) {
             setTheme(R.style.AppTheme_Dark);
         }
 
+        if (savedInstanceState != null && savedInstanceState.containsKey("themeChanged")) {
+            themeChanged = savedInstanceState.getBoolean("themeChanged");
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         ButterKnife.bind(this);
@@ -36,9 +43,27 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         darkModeSwitch.setChecked(MyApplication.getInstance().isDarkModeEnabled());
+
         darkModeSwitch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             MyApplication.getInstance().setDarkModeEnabled(isChecked);
+            this.themeChanged = !themeChanged; // If we go light -> dark -> light, we don't need to retheme
             recreate();
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        if (themeChanged) {
+            onNavigateUp();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        outState.putBoolean("themeChanged", themeChanged);
+        super.onSaveInstanceState(outState);
+    }
+
 }
