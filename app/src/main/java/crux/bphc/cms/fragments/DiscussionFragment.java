crux-bphc/CMS-Android

commit bb3d563e895bd0cbff4f5a32c98afc91010ac262

Refactor to Properly Encapsulate MyFileManager
MyFileManager has been refactored heavily to ensure proper encapsulation
and division of labour.

MyFileManager has been renamed to FileManager. The "My" suffix was
superflous.

The scope of generic  methods has been reduced and additional methods
have been created as necessary. As an example, instead of a generic
`downloadFile()` method to download files module Content as well as
discussion Attachment files, two new methods, `downloadModuleContent()`
and `downloadDiscussionAttachment()` has been added. Similar changes
have been made for the other operations as well.

The `onClickAction()` method has been removed and the code has been
refactored and placed inside CourseSectionFragment. The method had
nothing to do with file management and was completely related to the
managing modules.

app/src/main/java/crux/bphc/cms/fragments/DiscussionFragment.java
@@ -24,21 +24,21 @@ import java.util.Arrays;
 
 import crux.bphc.cms.R;
 import crux.bphc.cms.app.MyApplication;
+import crux.bphc.cms.helper.FileManager;
 import crux.bphc.cms.helper.HtmlTextView;
-import crux.bphc.cms.helper.MyFileManager;
 import crux.bphc.cms.helper.PropertiesAlertDialog;
-import io.realm.Realm;
 import crux.bphc.cms.models.forum.Attachment;
 import crux.bphc.cms.models.forum.Discussion;
+import io.realm.Realm;
 
 
-public class DiscussionFragment extends Fragment implements MyFileManager.Callback {
+public class DiscussionFragment extends Fragment {
 
-    private  String mFolderName;
+    private  String mCourseName;
     private int id;
 
     private Realm realm;
-    private MyFileManager mFileManager;
+    private FileManager mFileManager;
 
     private ImageView mUserPic;
     private TextView mSubject;
@@ -53,11 +53,11 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
         // Required empty public constructor
     }
 
-    public static DiscussionFragment newInstance(int id, String folderName) {
+    public static DiscussionFragment newInstance(int id, String mCourseName) {
         DiscussionFragment fragment = new DiscussionFragment();
         Bundle args = new Bundle();
         args.putInt("id", id);
-        args.putString("folderName", folderName);
+        args.putString("courseName", mCourseName);
         fragment.setArguments(args);
         return fragment;
     }
@@ -67,7 +67,7 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             id = getArguments().getInt("id");
-            mFolderName = getArguments().getString("folderName");
+            mCourseName = getArguments().getString("courseName");
         }
         realm = MyApplication.getInstance().getRealmInstance();
     }
@@ -85,11 +85,29 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
 
         moreOptionsViewModel = new ViewModelProvider(requireActivity()).get(MoreOptionsFragment.OptionsViewModel.class);
 
-        mFileManager = new MyFileManager(getActivity(), mFolderName);
-        mFileManager.registerDownloadReceiver();
-        mFileManager.setCallback(this);
-
         Discussion discussion = realm.where(Discussion.class).equalTo("id", id).findFirst();
+
+        mFileManager = new FileManager(getActivity(), mCourseName);
+        mFileManager.registerDownloadReceiver();
+        mFileManager.setCallback(filename -> {
+            int child = mAttachmentContainer.getChildCount();
+            for (int i = 0; i < child; i++) {
+                View childView = mAttachmentContainer.getChildAt(i);
+                TextView fileNameTextView = childView.findViewById(R.id.fileName);
+                if (fileNameTextView != null &&
+                        fileNameTextView.getText().toString().equalsIgnoreCase(filename)) {
+                    ImageView downloadIcon = childView.findViewById(R.id.downloadButton);
+                    downloadIcon.setImageResource(R.drawable.eye);
+                    ImageView ellipsis = childView.findViewById(R.id.more);
+                    ellipsis.setVisibility(View.VISIBLE);
+                    break;
+                }
+            }
+
+            Attachment attachment = discussion.getAttachments().where().equalTo("fileName", filename).findFirst();
+            if (attachment != null)
+                mFileManager.openDiscussionAttachment(attachment);
+        });
 
         mUserPic = view.findViewById(R.id.user_pic);
         Picasso.get().load(discussion.getUserpictureurl()).into(mUserPic);
@@ -124,7 +142,7 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
             ImageView download = attachmentView.findViewById(R.id.downloadButton);
             ImageView ellipsis = attachmentView.findViewById(R.id.more);
 
-            boolean downloaded = mFileManager.searchFile(attachment.getFilename());
+            boolean downloaded = mFileManager.isDiscussionAttachmentDownloaded(attachment);
             if (downloaded) {
                 download.setImageResource(R.drawable.eye);
                 ellipsis.setVisibility(View.VISIBLE);
@@ -136,15 +154,15 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
             clickWrapper.setOnClickListener(v -> {
                 if (!downloaded) {
                     Toast.makeText(getActivity(), "Downloading file - " + attachment.getFilename(), Toast.LENGTH_SHORT).show();
-                    mFileManager.downloadFile(attachment.getFilename(), attachment.getFileurl(), "", mFolderName, true);
+                    mFileManager.downloadDiscussionAttachment(attachment, discussion.getSubject(), mCourseName);
                 } else {
-                    mFileManager.openFile(attachment.getFilename());
+                    mFileManager.openDiscussionAttachment(attachment);
                 }
             });
 
             ellipsis.setOnClickListener(v -> {
                 // Check if downloaded once again, for consistency (user downloaded and then opens ellipsis immediately)
-                boolean isDownloaded = mFileManager.searchFile(attachment.getFilename());
+                boolean isDownloaded = mFileManager.isDiscussionAttachmentDownloaded(attachment);
                 if (isDownloaded) {
                     ArrayList<MoreOptionsFragment.Option> options = new ArrayList<>();
                     Observer<MoreOptionsFragment.Option> observer;  // to handle the selection
@@ -160,16 +178,16 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
                         if (option == null) return;
                         switch (option.getId()) {
                             case 0:
-                                mFileManager.openFile(attachment.getFilename());
+                                mFileManager.openDiscussionAttachment(attachment);
                                 break;
                             case 1:
                                 Toast.makeText(getActivity(), "Downloading file - " + attachment.getFilename(),
                                         Toast.LENGTH_SHORT).show();
-                                mFileManager.downloadFile(attachment.getFilename(), attachment.getFileurl(), "",
-                                        mFolderName, true);
+                                mFileManager.downloadDiscussionAttachment(attachment, discussion.getSubject(),
+                                        mCourseName);
                                 break;
                             case 2:
-                                mFileManager.shareFile(attachment.getFilename());
+                                mFileManager.shareDiscussionAttachment(attachment);
                                 break;
                             case 3:
                                 new PropertiesAlertDialog(getContext(), attachment).show();
@@ -191,23 +209,5 @@ public class DiscussionFragment extends Fragment implements MyFileManager.Callba
     public void onDestroy() {
         super.onDestroy();
         mFileManager.unregisterDownloadReceiver();
-    }
-
-    @Override
-    public void onDownloadCompleted(String fileName) {
-        int child = mAttachmentContainer.getChildCount();
-        for (int i = 0; i < child; i++) {
-            View childView = mAttachmentContainer.getChildAt(i);
-            TextView fileNameTextView = childView.findViewById(R.id.fileName);
-            if (fileNameTextView != null &&
-                    fileNameTextView.getText().toString().equalsIgnoreCase(fileName)) {
-                ImageView downloadIcon = childView.findViewById(R.id.downloadButton);
-                downloadIcon.setImageResource(R.drawable.eye);
-                ImageView ellipsis = childView.findViewById(R.id.more);
-                ellipsis.setVisibility(View.VISIBLE);
-                break;
-            }
-        }
-        mFileManager.openFile(fileName);
     }
 }

app/src/main/java/crux/bphc/cms/helper/FileManager.java
@@ -1,0 +1,335 @@
+package crux.bphc.cms.helper;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import crux.bphc.cms.BuildConfig;
+import crux.bphc.cms.app.Constants;
+import crux.bphc.cms.app.MyApplication;
+import crux.bphc.cms.models.Content;
+import crux.bphc.cms.models.Module;
+import crux.bphc.cms.models.forum.Attachment;
+
+/**
+ * <p>
+ * A manager class to manage file access and downloads of module Contents and
+ * discussion Attachments. An instance of this class is associated with a
+ * particular course.
+ * <p>
+ * On {@link Build.VERSION_CODES#P Android P} and below, files are downloaded to
+ * the location specified by {@link Environment#DIRECTORY_DOWNLOADS}.
+ * <p>
+ * Starting from {@link android.os.Build.VERSION_CODES#Q Android Q}, files are
+ * downloaded to the "primary" external storage volume ({@link
+ * MediaStore.Downloads#EXTERNAL_CONTENT_URI}) {@link MediaStore.Downloads}.
+ *
+ * @author Harshit Agarwal, Abhijeet Viswa
+ */
+public class FileManager {
+    public static final int DATA_DOWNLOADED = 20;
+
+    /**
+     * The folder into which files will be downloaded. This root folder will
+     * itself be inside {@link MediaStore.Downloads} or
+     * {@link Environment#DIRECTORY_DOWNLOADS}
+     */
+    private static final String ROOT_FOLDER = "CMS";
+
+    private List<String> fileList;
+    private Activity activity;
+    private ArrayList<String> requestedDownloads;
+    private Callback callback;
+    private String courseName;
+    private String courseDirName;
+    private BroadcastReceiver onComplete;
+
+    /**
+     * @param activity An Activity context, to launch new activities when
+     *                 opening files.
+     * @param courseName Course name the FileManager instance should be attached
+     *                   to. Only files inside the folder <code>{@linkplain
+     *                   #ROOT_FOLDER}/courseName</code> will be accessible from
+     *                   the given FileManager instance.
+     */
+    public FileManager(@NonNull Activity activity, @NonNull String courseName) {
+        this.activity = activity;
+        requestedDownloads = new ArrayList<>();
+        this.courseName = courseName;
+        this.courseDirName = getSanitizedRelativeCoursePath(courseName);
+
+        onComplete = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                reloadFileList();
+                for (String filename : requestedDownloads) {
+                    if (isFileDownloaded(filename)) {
+                        requestedDownloads.remove(filename);
+                        if (callback != null) {
+                            callback.onDownloadCompleted(filename);
+                        }
+                        return;
+                    }
+                }
+            }
+        };
+    }
+
+    public void downloadModuleContent(Content content, Module module) {
+        deleteExistingModuleContent(content);
+        downloadFile(content.getFilename(), content.getFileurl(), module.getDescription(),  courseName, false);
+    }
+
+    public void downloadDiscussionAttachment(Attachment attachment, String description, String courseName) {
+        deleteExistingDiscussionAttachment(attachment);
+        downloadFile(attachment.getFilename(), attachment.getFileurl(), description, courseName, true);
+    }
+
+    private void downloadFile(String fileName, String fileUrl, String description, String courseName, boolean isForum) {
+        String url = "";
+        if (isForum) {
+            url = fileUrl + "?token=" + Constants.TOKEN;
+        } else {
+            url = fileUrl + "&token=" + Constants.TOKEN;
+        }
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription(description);
+        request.setTitle(fileName);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,
+                getRelativeFilePath(courseName, fileName));
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            request.allowScanningByMediaScanner();
+        }
+
+        ((DownloadManager) MyApplication.getInstance().getSystemService(Context.DOWNLOAD_SERVICE))
+                .enqueue(request);
+    }
+
+    public void openModuleContent(Content content) {
+        openFile(content.getFilename());
+    }
+
+    public void openDiscussionAttachment(Attachment attachment) {
+        openFile(attachment.getFilename());
+    }
+
+    private void openFile(String filename) {
+        Uri fileUri = null;
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(),
+                    getRelativeFilePath(courseName, filename));
+            fileUri = FileProvider.getUriForFile(activity, BuildConfig.APPLICATION_ID + ".provider", file);
+        } else {
+            final Uri BASE_CONTENT_URI = MediaStore.Downloads.EXTERNAL_CONTENT_URI;
+
+            String[] projection = { MediaStore.Downloads._ID };
+            String where = "(" + MediaStore.Downloads.RELATIVE_PATH + " LIKE ?" + ") AND "
+                    + MediaStore.Downloads.DISPLAY_NAME + " = ?;";
+            String[] args = { "%" + getSanitizedCourseName(courseName) + "%", filename };
+            String order_by = MediaStore.Downloads.RELATIVE_PATH + " ASC";
+
+            try (Cursor cursor = MyApplication.getInstance().getContentResolver().query(
+                    BASE_CONTENT_URI,
+                    projection,
+                    where,
+                    args,
+                    order_by
+            )){
+                int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Downloads._ID);
+                if (cursor.moveToNext()) {
+                    fileUri = Uri.withAppendedPath(BASE_CONTENT_URI, "" + cursor.getInt(idColumn));
+                }
+            }
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(fileUri, FileUtils.getFileMimeType(filename));
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        try {
+            activity.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            intent.setDataAndType(fileUri, "application/*");
+            activity.startActivity(Intent.createChooser(intent, "No Application found to open File - " +
+                    filename));
+        }
+    }
+
+    public void shareModuleContent(Content content) {
+        shareFile(content.getFilename());
+    }
+
+    public void shareDiscussionAttachment(Attachment attachment) {
+        shareFile(attachment.getFilename());
+    }
+
+    private void shareFile(String filename) {
+        Uri fileUri = null;
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            String path = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()
+                    + getRelativeFilePath(courseName, filename);
+            fileUri = FileProvider.getUriForFile(activity, BuildConfig.APPLICATION_ID + ".provider", new File(path));
+        } else {
+            final Uri BASE_CONTENT_URI = MediaStore.Downloads.EXTERNAL_CONTENT_URI;
+
+            String[] projection = { MediaStore.Downloads._ID };
+            String where = "(" + MediaStore.Downloads.RELATIVE_PATH + " LIKE ?" + ") AND "
+                    + MediaStore.Downloads.DISPLAY_NAME + " = ?;";
+            String[] args = { "%" + getSanitizedCourseName(courseName) + "%", filename };
+            String order_by = MediaStore.Downloads.RELATIVE_PATH + " ASC";
+
+            try (Cursor cursor = MyApplication.getInstance().getContentResolver().query(
+                    BASE_CONTENT_URI,
+                    projection,
+                    where,
+                    args,
+                    order_by
+            )){
+                int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Downloads._ID);
+                if (cursor.moveToNext()) {
+                    fileUri = Uri.withAppendedPath(BASE_CONTENT_URI, "" + cursor.getInt(idColumn));
+                }
+            }
+        }
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        sendIntent.setType("application/*");
+
+        try {
+            activity.startActivity(Intent.createChooser(sendIntent, "Share File"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(activity, "No app found to share the file - " + filename, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void deleteExistingModuleContent(Content content) {
+        deleteExistingFile(content.getFilename());
+    }
+
+    public void deleteExistingDiscussionAttachment(Attachment attachment) {
+        deleteExistingFile(attachment.getFilename());
+    }
+
+    public void deleteExistingFile(String filename) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            String path = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()
+                    + getRelativeFilePath(courseName, filename);
+            File file = new File(path);
+            if (file.exists()) {
+                file.delete();
+            }
+        } else {
+            String where = "(" + MediaStore.Downloads.RELATIVE_PATH + " LIKE ?" + ") AND "
+                    + MediaStore.Downloads.DISPLAY_NAME + " = ?;";
+            String[] args = { "%" + getSanitizedCourseName(courseName) + "%", filename };
+
+            MyApplication.getInstance().getContentResolver().delete(
+                    MediaStore.Downloads.EXTERNAL_CONTENT_URI,
+                    where,
+                    args
+            );
+        }
+    }
+
+    public void reloadFileList() {
+        fileList = new ArrayList<>();
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            String path = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()
+                    + courseDirName;
+            File courseDir = new File(path);
+            if (courseDir.isDirectory()) {
+                fileList.addAll(Arrays.asList(courseDir.list()));
+            }
+        } else {
+            // MediaStore is backed by an SQLite database. We simply construct
+            // an SQL query clauses which the API will run on the database.
+            String[] projection = { MediaStore.Downloads.DISPLAY_NAME };
+            String where = MediaStore.Downloads.RELATIVE_PATH + " LIKE ?";
+            String[] args = { "%" + getSanitizedCourseName(courseName) + "%" };
+            String order_by = MediaStore.Downloads.RELATIVE_PATH + " ASC";
+
+            try (Cursor cursor = MyApplication.getInstance().getContentResolver().query(
+                    MediaStore.Downloads.EXTERNAL_CONTENT_URI,
+                    projection,
+                    where,
+                    args,
+                    order_by
+            )){
+                int nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Downloads.DISPLAY_NAME);
+                while (cursor.moveToNext()) {
+                    fileList.add(cursor.getString(nameColumn));
+                }
+            }
+        }
+    }
+
+    public boolean isModuleContentDownloaded(Content content) {
+        return isFileDownloaded(content.getFilename());
+    }
+
+    public boolean isDiscussionAttachmentDownloaded(Attachment attachment) {
+        return isFileDownloaded(attachment.getFilename());
+    }
+
+    private boolean isFileDownloaded(String fileName) {
+        if (fileList == null) {
+            reloadFileList();
+        }
+        return fileList.contains(fileName);
+    }
+
+    private String getRelativeFilePath(String courseName, String fileName) {
+        return getSanitizedRelativeCoursePath(courseName) + File.separator + fileName;
+    }
+
+    private String getSanitizedRelativeCoursePath(String courseName) {
+        return File.separator + ROOT_FOLDER + File.separator + getSanitizedCourseName(courseName);
+    }
+
+    private String getSanitizedCourseName(String courseName) {
+        return courseName.replaceAll("/", "_");
+    }
+
+    public void registerDownloadReceiver() {
+        activity.registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    public void unregisterDownloadReceiver() {
+        activity.unregisterReceiver(onComplete);
+    }
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+
+    public interface Callback {
+        void onDownloadCompleted(String fileName);
+    }
+}

commit 64a8bb60422ffcf2267acc2d97c6d4b40517242d

Fix: UI properly updates after file finishes downloading
Fixed regression introduced in bb3d563e895bd0cbff4f5a32c98afc91010ac262

app/src/main/java/crux/bphc/cms/fragments/DiscussionFragment.java
@@ -104,7 +104,7 @@ public class DiscussionFragment extends Fragment {
                 }
             }
 
-            Attachment attachment = discussion.getAttachments().where().equalTo("fileName", filename).findFirst();
+            Attachment attachment = discussion.getAttachments().where().equalTo("filename", filename).findFirst();
             if (attachment != null)
                 mFileManager.openDiscussionAttachment(attachment);
         });

app/src/main/java/crux/bphc/cms/helper/FileManager.java
@@ -120,6 +120,7 @@ public class FileManager {
             request.allowScanningByMediaScanner();
         }
 
+        requestedDownloads.add(fileName);
         ((DownloadManager) MyApplication.getInstance().getSystemService(Context.DOWNLOAD_SERVICE))
                 .enqueue(request);
     }