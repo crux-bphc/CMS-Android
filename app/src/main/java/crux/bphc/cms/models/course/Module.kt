crux-bphc/CMS-Android

commit 8a0a85b1b581906b5fc0f2d762249e4d7399f1e7

Kotlin-ify 'crux.bphc.cms.models.course'

app/src/main/java/crux/bphc/cms/models/course/Module.kt
@@ -1,0 +1,103 @@
+package crux.bphc.cms.models.course
+
+import androidx.core.text.HtmlCompat
+import com.google.gson.annotations.SerializedName
+import crux.bphc.cms.R
+import crux.bphc.cms.interfaces.CourseContent
+import crux.bphc.cms.utils.FileUtils
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.annotations.Ignore
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+/**
+ * @author Harshit Agarwal (16-Dec-2016)
+ * @author Abhijeet Viswa
+ */
+open class Module(
+        @PrimaryKey @SerializedName("id") var id: Int = 0,
+        @SerializedName("instance") var instance: Int = 0,
+        name: String = "",
+        @SerializedName("url") var url: String = "",
+        @SerializedName("modicon") var modIcon: String = "",
+        @SerializedName("modname") private var modName: String = "",
+        @SerializedName("description") var description: String = "",
+        @SerializedName("contents") var contents: RealmList<Content> = RealmList(),
+        var courseSectionId: Int = 0,
+        var isUnread: Boolean = false,
+) : RealmObject(), CourseContent {
+    enum class Type {
+        RESOURCE, FORUM, LABEL, ASSIGNMENT, FOLDER, QUIZ, URL, PAGE, DEFAULT, BOOK
+    }
+
+    @SerializedName("name") var name: String = name
+        get() {
+            return HtmlCompat.fromHtml(field, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
+                    .trim{ it <= ' ' }
+        }
+
+    @Ignore var modType: Type = Type.DEFAULT
+        get() {
+            if (field == Type.DEFAULT) field = inferModuleTypeFromModuleName()
+            return field
+        }
+
+    val isDownloadable: Boolean
+        get() = contents.isEmpty() && modType !in arrayOf(Type.URL, Type.FORUM, Type.PAGE)
+
+    val moduleIcon: Int
+        get() {
+            return when (modType) {
+                Type.RESOURCE -> if (contents.isEmpty()) {
+                    val content = contents.first() ?: return -1
+                    FileUtils.getDrawableIconFromFileName(content.fileName)
+                } else {
+                    -1
+                }
+                Type.ASSIGNMENT -> R.drawable.assignment
+                Type.FOLDER -> R.drawable.folder
+                Type.URL -> R.drawable.web
+                Type.PAGE -> R.drawable.page
+                Type.QUIZ -> R.drawable.quiz
+                Type.FORUM -> R.drawable.forum
+                Type.BOOK -> R.drawable.book
+                Type.DEFAULT, Type.LABEL -> -1
+            }
+        }
+
+    fun deepCopy(): Module = Module(
+            id,
+            instance,
+            name,
+            url,
+            modIcon,
+            modName,
+            description,
+            RealmList<Content>(*contents.map { it.copy() }.toTypedArray()),
+            courseSectionId,
+            isUnread,
+    )
+
+    private fun inferModuleTypeFromModuleName(): Type {
+        return when (modName.toLowerCase(Locale.ROOT)) {
+            "resource" -> Type.RESOURCE
+            "forum" -> Type.FORUM
+            "label" -> Type.LABEL
+            "assign" -> Type.ASSIGNMENT
+            "folder" -> Type.FOLDER
+            "quiz" -> Type.QUIZ
+            "url" -> Type.URL
+            "page" -> Type.PAGE
+            else -> Type.DEFAULT
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is Module && other.id == id
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
+}

app/src/main/java/crux/bphc/cms/activities/CourseDetailActivity.java
@@ -106,7 +106,7 @@ public class CourseDetailActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         CourseContentFragment courseSectionFragment = CourseContentFragment.newInstance(
                 TOKEN,
-                course.getCourseId());
+                course.getId());
         fragmentTransaction.replace(
                 R.id.course_section_enrol_container,
                 courseSectionFragment,

app/src/main/java/crux/bphc/cms/activities/MainActivity.kt
@@ -110,7 +110,7 @@ class MainActivity : AppCompatActivity() {
             }
             var isEnrolled = false
             for (course in courses) {
-                if (course.courseId == courseId) {
+                if (course.id == courseId) {
                     isEnrolled = true
                     break
                 }

app/src/main/java/crux/bphc/cms/activities/TokenActivity.java
@@ -7,7 +7,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -294,7 +293,7 @@ public class TokenActivity extends AppCompatActivity {
                         }
                     }
                 }
-                courseDataHandler.replaceCourseData(course.getCourseId(), courseSections);
+                courseDataHandler.replaceCourseData(course.getId(), courseSections);
             }
 
             /* Fetch Site News */

app/src/main/java/crux/bphc/cms/adapters/delegates/CourseModuleDelegate.java
@@ -100,7 +100,7 @@ public class CourseModuleDelegate extends AdapterDelegate<List<CourseContent>> {
 
     private void bindNameAndDescription(CourseModuleViewHolder vh, Module module) {
         vh.name.setText(module.getName());
-        if (module.getDescription() != null && !module.getDescription().isEmpty()) {
+        if (!module.getDescription().isEmpty()) {
             vh.description.setVisibility(View.VISIBLE);
             vh.nameAndDescriptionDivider.setVisibility(View.VISIBLE);
             vh.description.setFullText(new SpannableStringBuilder()

app/src/main/java/crux/bphc/cms/fragments/CourseContentFragment.java
@@ -33,17 +33,17 @@ import crux.bphc.cms.R;
 import crux.bphc.cms.adapters.CourseContentAdapter;
 import crux.bphc.cms.app.Constants;
 import crux.bphc.cms.app.MyApplication;
-import crux.bphc.cms.interfaces.ClickListener;
 import crux.bphc.cms.helper.CourseDataHandler;
 import crux.bphc.cms.helper.CourseRequestHandler;
-import crux.bphc.cms.io.FileManager;
-import crux.bphc.cms.widgets.PropertiesAlertDialog;
-import crux.bphc.cms.utils.Utils;
+import crux.bphc.cms.interfaces.ClickListener;
 import crux.bphc.cms.interfaces.CourseContent;
+import crux.bphc.cms.io.FileManager;
 import crux.bphc.cms.models.course.Content;
 import crux.bphc.cms.models.course.CourseSection;
 import crux.bphc.cms.models.course.Module;
 import crux.bphc.cms.models.forum.Discussion;
+import crux.bphc.cms.utils.Utils;
+import crux.bphc.cms.widgets.PropertiesAlertDialog;
 import io.realm.Realm;
 
 import static crux.bphc.cms.io.FileManager.DATA_DOWNLOADED;
@@ -292,14 +292,13 @@ public class CourseContentFragment extends Fragment {
                 case URL:
                     if (activity != null && content != null) {
                         String url = content.getFileUrl();
-                        if (url != null && !url.isEmpty()) {
+                        if (!url.isEmpty()) {
                             Utils.openURLInBrowser(activity, url);
                         }
                     }
                     break;
                 case PAGE:
-                    String url = module.getUrl();
-                    if (activity != null && url != null) {
+                    if (activity != null) {
                         Utils.openURLInBrowser(activity, module.getUrl());
                     }
                     break;
@@ -318,7 +317,7 @@ public class CourseContentFragment extends Fragment {
                     break;
                 case LABEL:
                     String desc = module.getDescription();
-                    if (activity != null && desc != null && !desc.isEmpty()) {
+                    if (activity != null && !desc.isEmpty()) {
 
                         AlertDialog.Builder alertDialog;
                         if (MyApplication.getInstance().isDarkModeEnabled()) {
@@ -436,7 +435,7 @@ public class CourseContentFragment extends Fragment {
     private List<CourseContent> getCourseContents() {
         ArrayList<CourseContent> contents = new ArrayList<>();
         courseSections.stream().filter(courseSection -> !(courseSection.getModules().isEmpty()
-                && (courseSection.getSummary() == null || courseSection.getSummary().isEmpty())
+                && courseSection.getSummary().isEmpty()
                 && (courseSection.getName().matches("Topic \\d"))
         )).forEach(courseSection -> {
             contents.add(courseSection);

app/src/main/java/crux/bphc/cms/fragments/MyCoursesFragment.kt
@@ -85,7 +85,7 @@ class MyCoursesFragment : Fragment() {
                     val courseDataHandler = CourseDataHandler(requireContext(), realm)
 
                     for (course in courses) {
-                        val sections = courseDataHandler.getCourseData(course.courseId)
+                        val sections = courseDataHandler.getCourseData(course.id)
                         courseDataHandler.markAllAsRead(sections)
                     }
 
@@ -115,7 +115,7 @@ class MyCoursesFragment : Fragment() {
         mAdapter.clickListener = ClickListener { `object`: Any, _: Int ->
             val course = `object` as Course
             val intent = Intent(activity, CourseDetailActivity::class.java)
-            intent.putExtra("courseId", course.courseId)
+            intent.putExtra("courseId", course.id)
             intent.putExtra("course_name", course.shortName)
             courseDetailActivityLauncher.launch(intent)
             return@ClickListener true
@@ -158,7 +158,7 @@ class MyCoursesFragment : Fragment() {
                     courseDownloader.unregisterReceiver()
                 }
             })
-            courseDownloader.downloadCourseData(course.courseId)
+            courseDownloader.downloadCourseData(course.id)
             return@ClickListener true
         }
 
@@ -245,9 +245,9 @@ class MyCoursesFragment : Fragment() {
                 async innerAsync@{
                     val sections: MutableList<CourseSection>
                     try {
-                        sections = courseRequestHandler.getCourseDataSync(it.courseId)
+                        sections = courseRequestHandler.getCourseDataSync(it.id)
                     } catch (e: IOException) {
-                        Log.e(TAG, "IOException when syncing course: ${it.courseId}}", e)
+                        Log.e(TAG, "IOException when syncing course: ${it.id}}", e)
                         return@innerAsync false
                     }
 
@@ -268,8 +268,8 @@ class MyCoursesFragment : Fragment() {
                     }
 
                     val newPartsInSections = courseDataHandler
-                            .isolateNewCourseData(it.courseId, sections)
-                    courseDataHandler.replaceCourseData(it.courseId, sections)
+                            .isolateNewCourseData(it.id, sections)
+                    courseDataHandler.replaceCourseData(it.id, sections)
 
                     realm.close() // let's not forget to do this
                     if (newPartsInSections.isNotEmpty()) {
@@ -354,7 +354,7 @@ class MyCoursesFragment : Fragment() {
 
         inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             fun bind(course: Course) {
-                val name = course.courseName[1].toString() + " " + course.courseName[2]
+                val name = course.courseName[1] + " " + course.courseName[2]
                 val count = courseDataHandler.getUnreadCount(course.id)
                 itemView.course_number.text = course.courseName[0]
                 itemView.course_name.text = name
@@ -381,7 +381,7 @@ class MyCoursesFragment : Fragment() {
             }
 
             fun markAllAsRead(position: Int) {
-                val courseId = this@MyCoursesFragment.courses[position].courseId
+                val courseId = this@MyCoursesFragment.courses[position].id
                 val courseSections: List<CourseSection>
 
                 courseSections = courseDataHandler.getCourseData(courseId)
@@ -395,7 +395,7 @@ class MyCoursesFragment : Fragment() {
 
             fun setFavoriteStatus(position: Int, isFavourite: Boolean) {
                 val course = courses[position]
-                courseDataHandler.setFavoriteStatus(course.courseId, isFavourite)
+                courseDataHandler.setFavoriteStatus(course.id, isFavourite)
                 course.isFavorite = isFavourite
                 sortCourses(courses)
                 notifyDataSetChanged()

app/src/main/java/crux/bphc/cms/helper/CourseDataHandler.java
@@ -21,6 +21,7 @@ import crux.bphc.cms.models.course.Module;
 import crux.bphc.cms.models.forum.Discussion;
 import io.realm.Realm;
 
+import io.realm.RealmList;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
@@ -193,7 +194,7 @@ public class CourseDataHandler {
     private List<CourseSection> isolateNewAndModifiedModulesInSections(@NonNull List<CourseSection> sections) {
         List<CourseSection> retSections = new ArrayList<>();
         for (CourseSection section : sections) {
-            CourseSection retSection = new CourseSection(section);
+            CourseSection retSection = section.deepCopy();
             List<Module> modules = section.getModules();
             List<Module> retModules = new ArrayList<>();
 
@@ -206,7 +207,7 @@ public class CourseDataHandler {
             for (Module module : modules) {
                 if (!inDBIds.contains(module.getId())) {
                     // Add the modules that are brand new
-                    module.setIsUnread(true);
+                    module.setUnread(true);
                     retModules.add(module);
                 } else {
                     Module mod = isolateNewContentInModules(module);
@@ -216,7 +217,7 @@ public class CourseDataHandler {
                 }
             }
             if (!retModules.isEmpty()) {
-                retSection.setModules(retModules);
+                retSection.setModules(new RealmList<>(retModules.toArray(new Module[0])));
                 retSections.add(retSection);
             }
         }
@@ -239,14 +240,14 @@ public class CourseDataHandler {
         Module realmModule = realm.where(Module.class).equalTo("id", module.getId()).findFirst();
 
         if (realmModule == null) {
-            module.setIsUnread(true);
+            module.setUnread(true);
             return module; // Everything is new
         }
 
         // Always copy over flag in case user manually set unread and for it
         // to persist across multiple data requests, irrespective of whether
         // content is new or not
-        module.setIsUnread(realmModule.isUnread());
+        module.setUnread(realmModule.isUnread());
 
         RealmResults<Content> results = realm.where(Content.class)
                 .in("fileName", module.getContents().stream().map(Content::getFileName).toArray(String[]::new))
@@ -255,14 +256,14 @@ public class CourseDataHandler {
                 .findAll();
 
         List<Content> newContents = module.getContents().stream().filter(content -> !results.contains(content))
-                .map(Content::new).collect(Collectors.toList());
+                .map(Content::copy).collect(Collectors.toList());
         if (newContents.isEmpty()) {
             return null;
         }
 
-        module.setIsUnread(true);
-        Module retModule = new Module(module);
-        retModule.setContents(newContents);
+        module.setUnread(true);
+        Module retModule = module.deepCopy();
+        retModule.setContents(new RealmList<>(newContents.toArray(new Content[0])));
         return retModule;
     }
 
@@ -313,7 +314,7 @@ public class CourseDataHandler {
     }
 
     public void markModuleAsReadOrUnread(Module module, boolean isUnread) {
-        module.setIsUnread(isUnread);
+        module.setUnread(isUnread);
         boolean endTransaction = false;
 
         if (!realm.isInTransaction()) {
@@ -322,7 +323,7 @@ public class CourseDataHandler {
         }
 
         Module mod = realm.where(Module.class).equalTo("id", module.getId()).findFirst();
-        if (mod != null) mod.setIsUnread(isUnread);
+        if (mod != null) mod.setUnread(isUnread);
 
         if (endTransaction)
         {
@@ -333,7 +334,7 @@ public class CourseDataHandler {
     public String getCourseName(int courseId) {
         Course course = realm.where(Course.class).equalTo("id", courseId).findFirst();
         if (course == null) return "";
-        return course.getShortName() != null ? course.getShortName() : "";
+        return course.getShortName();
     }
 
     public String getCourseNameForActionBarTitle(int courseId){

commit af1939973600b4e520d00d62b23af60ad97e2d08

Fix: Downloadable modules no longer downloadable
Modules which had contents (for eg. Resource modules) weren't
downloadable. Bug Introduced in
`8a0a85b1b581906b5fc0f2d762249e4d7399f1e7`

app/src/main/java/crux/bphc/cms/models/course/Module.kt
@@ -44,7 +44,7 @@ open class Module(
         }
 
     val isDownloadable: Boolean
-        get() = contents.isEmpty() && modType !in arrayOf(Type.URL, Type.FORUM, Type.PAGE)
+        get() = contents.isNotEmpty() && modType !in arrayOf(Type.URL, Type.FORUM, Type.PAGE)
 
     val moduleIcon: Int
         get() {