crux-bphc/CMS-Android

commit c18f41121c3fbe090b56697a3045615d00027125

Switch to WorkManager from JobScheduler for NotificationService
WorkManager is part of Jetpack and is the Android recommended way to
schedule periodic work in the background.

This commit cleans up the erstwhile NotificationService.java file,
Kotlin-ifying and refactoring it. InboxStyle has been replaced with
BigTextStyle notifications on Oreo and above.

In cases where a logout happens due to InvalidToken or any other reason,
a notification is shown to the user. This closes #241.

app/src/main/java/crux/bphc/cms/models/NotificationSet.kt
@@ -1,0 +1,94 @@
+package crux.bphc.cms.models
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import crux.bphc.cms.activities.MainActivity
+import crux.bphc.cms.app.Constants
+import crux.bphc.cms.models.course.Course
+import crux.bphc.cms.models.course.CourseSection
+import crux.bphc.cms.models.course.Module
+import crux.bphc.cms.models.forum.Discussion
+
+/**
+ * A data class for notification data
+ *
+ * @author Harshit Agarwal (18-Jan-2017)
+ * @author Abhijeet Viswa
+ */
+data class NotificationSet(
+        val uniqueId: Int,
+        val bundleId: Int,
+        val notifTitle: String,
+        val notifContent: String,
+        val groupKey: String?,
+        val pendingIntent: PendingIntent?
+) {
+    companion object {
+        /**
+         * Helper method to create [NotificationSet] object for a new module
+         */
+        fun createNotificationSetForModule(context: Context, course: Course, section: CourseSection,
+                                           module: Module)
+                : NotificationSet {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.putExtra("courseId", course.id)
+            intent.putExtra("modId", module.id)
+
+            val pendingIntent = PendingIntent.getActivity(
+                    context,
+                    System.currentTimeMillis().toInt(),
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            return NotificationSet(module.id, course.id, section.name, module.name,
+                    course.shortName, pendingIntent)
+        }
+
+        /**
+         * Helper method to create [NotificationSet] object for a new discussion
+         */
+        fun createNotificationSetForDiscussion(context: Context, course: Course, module: Module,
+                                               discussion: Discussion)
+                : NotificationSet {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.putExtra("courseId", course.id)
+            intent.putExtra("modId", module.id)
+            intent.putExtra("forumId", module.instance)
+            intent.putExtra("discussionId", discussion.id)
+
+            val pendingIntent = PendingIntent.getActivity(
+                    context,
+                    System.currentTimeMillis().toInt(),
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            return NotificationSet(discussion.id, course.id, module.name, discussion.message,
+                   course.shortName, pendingIntent)
+        }
+
+        fun createNotificationSetForSiteNews(context: Context, discussion: Discussion)
+               : NotificationSet {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.putExtra("courseId", Constants.SITE_NEWS_COURSE_ID)
+            intent.putExtra("forumId", 1)
+            intent.putExtra("discussionId", discussion.id)
+
+            val pendingIntent = PendingIntent.getActivity(
+                    context,
+                    System.currentTimeMillis().toInt(),
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            return NotificationSet(1, 0, "Site News", discussion.message,
+                    "Site News", pendingIntent)
+        }
+    }
+}
+

app/build.gradle
@@ -69,6 +69,7 @@ dependencies {
     implementation 'androidx.fragment:fragment-ktx:1.3.0-beta01'
     implementation 'androidx.vectordrawable:vectordrawable:1.1.0'
     implementation 'androidx.preference:preference:1.1.1'
+    implementation "androidx.work:work-runtime-ktx:2.4.0"
     // Network requests library
     implementation 'com.squareup.retrofit2:retrofit:2.9.0'
     implementation 'com.squareup.retrofit2:converter-gson:2.9.0'

app/src/main/AndroidManifest.xml
@@ -60,11 +60,6 @@
             android:name=".activities.CourseDetailActivity"
             android:screenOrientation="portrait" />
 
-        <service
-            android:name=".services.NotificationService"
-            android:exported="true"
-            android:permission="android.permission.BIND_JOB_SERVICE" />
-
         <provider
             android:name="androidx.core.content.FileProvider"
             android:authorities="${applicationId}.provider"

app/src/main/java/crux/bphc/cms/activities/MainActivity.kt
@@ -14,19 +14,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import crux.bphc.cms.R
 import crux.bphc.cms.app.Constants
 import crux.bphc.cms.app.MyApplication
+import crux.bphc.cms.background.NotificationWorker
 import crux.bphc.cms.fragments.*
+import crux.bphc.cms.helper.NOTIFICATION_CHANNEL_UPDATES
+import crux.bphc.cms.helper.NOTIFICATION_CHANNEL_UPDATES_BUNDLE
 import crux.bphc.cms.models.UserAccount
 import crux.bphc.cms.models.course.Course
 import crux.bphc.cms.models.course.CourseSection
-import crux.bphc.cms.models.course.Module
-import crux.bphc.cms.services.NotificationService
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -78,7 +83,11 @@ class MainActivity : AppCompatActivity() {
 
         askPermission()
         createNotificationChannels() // initialize channels before starting background service
-        NotificationService.startService(this, false)
+        // Enqueue background worker for course updates and notifications
+        val notifWorkRequest = PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.HOURS)
+                .build()
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork("NotificationWorker",
+                ExistingPeriodicWorkPolicy.KEEP, notifWorkRequest)
         resolveIntent()
         resolveModuleLinkShare()
     }
@@ -136,13 +145,13 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create the "Updates Bundle" channel, which is channel for summary notifications that bundles thr
             // individual notifications
-            val service = NotificationChannel(NotificationService.NOTIFICATION_CHANNEL_UPDATES_BUNDLE,
+            val service = NotificationChannel(NOTIFICATION_CHANNEL_UPDATES_BUNDLE,
                     "New Content Bundle",
                     NotificationManager.IMPORTANCE_DEFAULT)
             service.description = "A default priority channel that bundles all the notifications"
 
             // Create the "Updates" channel which has the low importance level
-            val updates = NotificationChannel(NotificationService.NOTIFICATION_CHANNEL_UPDATES,
+            val updates = NotificationChannel(NOTIFICATION_CHANNEL_UPDATES,
                     "New Content",
                     NotificationManager.IMPORTANCE_LOW)
             updates.description = "All low importance channel that relays all the updates."
@@ -156,43 +165,53 @@ class MainActivity : AppCompatActivity() {
     private fun resolveIntent() {
         val courseId = intent.getIntExtra("courseId", -1)
         val modId = intent.getIntExtra("modId", -1)
+        val forumId = intent.getIntExtra("forumId", -1)
+        val discussionId = intent.getIntExtra("discussionId", -1)
+
         if (courseId == -1) return
-        if (courseId == 1) {
+        if (courseId == Constants.SITE_NEWS_COURSE_ID) {
             // Site news, modId will not be -1
             // We will push the fragment here itself
-            val forumFragment: Fragment = ForumFragment.newInstance()
+            val forumFragment = ForumFragment.newInstance()
             pushView(forumFragment, "Site News", false)
 
             // Ensure that the fragment has been commited
             supportFragmentManager.executePendingTransactions()
-            val discussionFragment: Fragment = DiscussionFragment.newInstance(modId, "Site News")
+            val discussionFragment = DiscussionFragment.newInstance(discussionId, "Site News")
             pushView(discussionFragment, "Discussion", false)
         }
+
         if (modId == -1) {
             val intent = Intent(this, CourseDetailActivity::class.java)
             intent.putExtra("courseId", courseId)
             startActivity(intent)
             return
         }
+
+        if (discussionId != -1) {
+            // Open up the discussion first
+            val intent = Intent(this, CourseDetailActivity::class.java)
+            intent.putExtra("courseId", courseId)
+            intent.putExtra("modId", modId)
+            intent.putExtra("forumId", forumId)
+            intent.putExtra("discussionId", discussionId)
+            startActivity(intent)
+            return
+        }
+
         val realm = Realm.getDefaultInstance()
         val courseSections = realm.copyFromRealm(realm.where(CourseSection::class.java)
                 .equalTo("courseId", courseId).findAll())
         if (courseSections == null || courseSections.isEmpty()) return
-        for (courseSection in courseSections) {
-            for (module in courseSection.modules) {
-                if (module.id == modId) {
-                    val intent = Intent(this, CourseDetailActivity::class.java)
-                    intent.putExtra("courseId", courseId)
-                    if (!module.isDownloadable && module.modType == Module.Type.FORUM) {
-                        intent.putExtra("forumId", module.instance)
-                    }
-                    startActivity(intent)
-                    return
-                }
+        for (module in courseSections.flatMap { it.modules }) {
+            if (module.id == modId) {
+                val intent = Intent(this, CourseDetailActivity::class.java)
+                intent.putExtra("courseId", courseId)
+                intent.putExtra("modId", modId)
+                startActivity(intent)
+                return
             }
         }
-
-        // If we've gotten this far, we need to open a discussion
         val intent = Intent(this, CourseDetailActivity::class.java)
         intent.putExtra("courseId", courseId)
         intent.putExtra("discussionId", modId)

app/src/main/java/crux/bphc/cms/activities/TokenActivity.java
@@ -230,8 +230,6 @@ public class TokenActivity extends AppCompatActivity {
     private void checkLoggedIn() {
         if (userAccount.isLoggedIn()) {
             Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("courseId", getIntent().getIntExtra("courseId", -1));
-            intent.putExtra("modId", getIntent().getIntExtra("modId", -1));
             dismissProgress();
             startActivity(intent);
             finish();

app/src/main/java/crux/bphc/cms/app/Constants.java
@@ -33,6 +33,11 @@ public class Constants {
 
     }
 
+    /**
+     * Site News isn't part of any course. However, internally we assume it to be part of course 0.
+     */
+    public static int SITE_NEWS_COURSE_ID = 0;
+
     public static String getCourseURL(int courseId) {
         return COURSE_URL + "?id=" + courseId;
     }

app/src/main/java/crux/bphc/cms/background/NotificationWorker.kt
@@ -1,0 +1,115 @@
+package crux.bphc.cms.background
+
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import crux.bphc.cms.exceptions.InvalidTokenException
+import crux.bphc.cms.helper.*
+import crux.bphc.cms.models.UserAccount
+import crux.bphc.cms.models.course.Course
+import crux.bphc.cms.models.course.Module
+import io.realm.Realm
+
+
+class NotificationWorker(context: Context, workerParam: WorkerParameters):
+        Worker(context, workerParam) {
+
+    override fun doWork(): Result {
+        println("test")
+        Log.d(TAG, "Started syncing course data")
+        val userAccount = UserAccount(applicationContext)
+
+        // course data can't be accessed without user login, so cancel jobs if they're not logged in
+        if (!userAccount.isLoggedIn) {
+            return Result.failure()
+        }
+
+        val realm = Realm.getDefaultInstance()
+        val courseDataHandler = CourseDataHandler(applicationContext, realm)
+        val courseRequestHandler = CourseRequestHandler(applicationContext)
+        val notifManager = applicationContext.getSystemService(NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        // fetches list of enrolled courses from server
+        val courseList: MutableList<Course> = emptyList<Course>().toMutableList()
+        try {
+            courseRequestHandler.fetchCourseListSync().filterNotNull().forEach() {
+                it -> courseList.add(it)
+            }
+         } catch (e: InvalidTokenException) {
+            Log.e(TAG, "Invalid User Token");
+            createLoggedOutNotif(applicationContext, notifManager)
+            userAccount.logout()
+            return Result.failure()
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception", e)
+            return Result.retry() // The next time things will be fine
+        }
+
+        // replace the list of courses in db, and get new inserts
+        val newCourses = courseDataHandler.isolateNewCourses(courseList)
+        courseDataHandler.replaceCourses(courseList)
+        for (course in courseList) {
+            val courseSections = courseRequestHandler.getCourseData(course) ?: continue
+
+            /* Update the sections of the course, and get new parts. Since new course notifications
+               are skipped, default modules like "Announcements" will not get a notif.
+             */
+            val newPartsInSection = courseDataHandler.isolateNewCourseData(course.id,
+                    courseSections)
+            courseDataHandler.replaceCourseData(course.id, courseSections)
+
+            // Generate notifications only if it is not a new course
+            if (newCourses.contains(course)) continue
+            for (courseSection in courseSections) {
+                val modules: List<Module> = courseSection.modules
+                for (module in modules) {
+                    if (module.modType === Module.Type.FORUM) {
+                        val discussions = courseRequestHandler.getForumDiscussions(module.instance)
+                                ?: continue
+                        for (d in discussions) {
+                            d.forumId = module.instance
+                        }
+
+                        val newDiscussions = courseDataHandler.setForumDiscussions(module.instance,
+                                discussions)
+                        if (newDiscussions.size > 0){
+                            courseDataHandler.markModuleAsReadOrUnread(module, true)
+                        }
+                        for (discussion in newDiscussions) {
+                            pushDiscussionNotif(applicationContext, notifManager, discussion,
+                                    module, course)
+                        }
+                    }
+                }
+            }
+
+        for (section in newPartsInSection)
+            pushCourseSectionNotif(applicationContext, notifManager, section, course)
+        }
+
+        // Create notifications for site news
+        val discussions = courseRequestHandler.getForumDiscussions(1) // 1 is always site news
+        if (discussions != null) {
+            for (d in discussions) {
+                d.forumId = 1
+            }
+            val newDiscussions = courseDataHandler.setForumDiscussions(1, discussions)
+            for (discussion in newDiscussions) {
+                pushSiteNewsNotif(applicationContext, notifManager, discussion)
+            }
+        }
+        realm.close()
+        return Result.success()
+    }
+
+    companion object {
+        const val TAG = "NotificationWorker"
+    }
+
+}
+
+

app/src/main/java/crux/bphc/cms/helper/NotificationHelper.kt
@@ -1,0 +1,96 @@
+@file:JvmName("NotificationHelper")
+@file:JvmMultifileClass
+
+package crux.bphc.cms.helper
+
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import crux.bphc.cms.R
+import crux.bphc.cms.models.NotificationSet
+import crux.bphc.cms.models.course.Course
+import crux.bphc.cms.models.course.CourseSection
+import crux.bphc.cms.models.course.Module
+import crux.bphc.cms.models.forum.Discussion
+import crux.bphc.cms.widgets.HtmlTextView
+
+const val NOTIFICATION_CHANNEL_UPDATES_BUNDLE: String = "channel_content_updates_bundle"
+const val NOTIFICATION_CHANNEL_UPDATES: String = "channel_content_updates"
+
+fun pushCourseSectionNotif(context: Context, notifManager: NotificationManager,
+                           section: CourseSection, course: Course) {
+    for (module in section.modules) {
+        pushModuleNotif(context, notifManager, module, section, course)
+    }
+}
+
+fun pushModuleNotif(context: Context, notifManager: NotificationManager, module: Module,
+                    section: CourseSection, course:Course) {
+   createNotifFromSet(context, notifManager,
+           NotificationSet.createNotificationSetForModule(context, course, section, module))
+}
+
+fun pushDiscussionNotif(context: Context, notifManager: NotificationManager,
+                        discussion: Discussion, module: Module, course: Course) {
+      createNotifFromSet(context, notifManager,
+        NotificationSet.createNotificationSetForDiscussion(context, course, module, discussion))
+}
+
+fun pushSiteNewsNotif(context: Context, notifManager: NotificationManager, discussion: Discussion) {
+    createNotifFromSet(context, notifManager,
+        NotificationSet.createNotificationSetForSiteNews(context, discussion))
+}
+
+fun createLoggedOutNotif(context: Context, notifManager: NotificationManager) {
+    val notifSet = NotificationSet(
+            System.currentTimeMillis().toInt(),
+            System.currentTimeMillis().toInt(),
+            context.getString(R.string.logout_notif_title),
+            context.getString(R.string.logout_notif_content),
+            null,
+            null
+    )
+    createNotifFromSet(context, notifManager, notifSet)
+}
+
+fun createNotifFromSet(context: Context, notifManager: NotificationManager,
+                       notifSet: NotificationSet) {
+    // channel ID is ignored for below Oreo
+    val mBuilder = NotificationCompat
+            .Builder(context, NOTIFICATION_CHANNEL_UPDATES)
+            .setSmallIcon(R.drawable.ic_bits_logo)
+            .setGroup(notifSet.groupKey)
+            .setGroupSummary(false)
+            .setAutoCancel(true)
+            .setContentIntent(notifSet.pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        // Build the summary notification for Noutgat+ devices
+        val groupBuilder = NotificationCompat
+                .Builder(context, NOTIFICATION_CHANNEL_UPDATES_BUNDLE)
+                .setSmallIcon(R.drawable.ic_bits_logo)
+                .setContentText(HtmlTextView.parseHtml(notifSet.groupKey))
+                .setStyle(NotificationCompat.InboxStyle()
+                        .setBigContentTitle(notifSet.groupKey)
+                        .setSummaryText(notifSet.groupKey))
+                .setGroup(notifSet.groupKey)
+                .setGroupSummary(true)
+                .setAutoCancel(true)
+                .setContentIntent(notifSet.pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setOnlyAlertOnce(true)
+        notifManager.notify(notifSet.bundleId, groupBuilder.build())
+
+        mBuilder.setContentTitle(notifSet.notifTitle)
+                .setContentText(notifSet.notifContent)
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .setSummaryText(notifSet.groupKey)
+                        .bigText(notifSet.notifContent))
+    } else {
+        mBuilder.setContentTitle(notifSet.groupKey)
+                .setContentText(notifSet.notifContent)
+    }
+    notifManager.notify(notifSet.uniqueId, mBuilder.build())
+}

commit f0838b541fa78b7d9bfeb6b6d30950440877f704

Fix: Unparsed HTML tags in Discussion notifications
Regression from `c18f4112`.

Closes #261

app/src/main/java/crux/bphc/cms/fragments/DiscussionFragment.java
@@ -119,7 +119,7 @@ public class DiscussionFragment extends Fragment {
             mSubject.setText(discussion.getSubject());
             mUserName.setText(discussion.getUserFullName());
             mTimeModified.setText(ForumFragment.formatDate(discussion.getTimeModified()));
-            mMessage.setText(HtmlTextView.parseHtml(discussion.getMessage()));
+            mMessage.setText(discussion.getMessage());
 
             LayoutInflater inflater = LayoutInflater.from(requireContext());
             if (attachments != null && !attachments.isEmpty()) {

app/src/main/java/crux/bphc/cms/fragments/ForumFragment.java
@@ -277,7 +277,7 @@ public class ForumFragment extends Fragment {
                 mSubject.setText(discussion.getSubject());
                 mUserName.setText(discussion.getUserFullName());
                 mModifiedTime.setText(formatDate(discussion.getTimeModified()));
-                mMessage.setText(HtmlTextView.parseHtml((discussion.getMessage())));
+                mMessage.setText(discussion.getMessage());
                 if(!discussion.isPinned()){
                     mPinned.setVisibility(View.GONE);
                 }

app/src/main/java/crux/bphc/cms/models/NotificationSet.kt
@@ -68,7 +68,7 @@ data class NotificationSet(
                     PendingIntent.FLAG_UPDATE_CURRENT
             )
             return NotificationSet(discussion.id, course.id, module.name, discussion.message,
-                   course.shortName, pendingIntent)
+                    course.shortName, pendingIntent)
         }
 
         fun createNotificationSetForSiteNews(context: Context, discussion: Discussion)

app/src/main/java/crux/bphc/cms/models/forum/Discussion.kt
@@ -45,10 +45,7 @@ open class Discussion(
 
         subject: String = "" ,
 
-        /**
-         * The content of this discussion.
-         */
-        @SerializedName("message") var message: String = "",
+        message: String = "",
 
         /**
          * List of attachments of this post
@@ -78,6 +75,16 @@ open class Discussion(
 ) : RealmObject() {
 
     var subject: String = subject
+        get() {
+            return HtmlCompat.fromHtml(field, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
+                    .trim { it < ' ' }
+        }
+        private set
+
+    /**
+     * The content of this discussion.
+     */
+    @SerializedName("message") var message: String = ""
         get() {
             return HtmlCompat.fromHtml(field, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
                     .trim { it < ' ' }