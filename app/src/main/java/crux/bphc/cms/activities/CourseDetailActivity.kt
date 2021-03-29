crux-bphc/CMS-Android

commit b4a7710d49f684ae2d0fe44d0a52c4e98dbae469

Fix: NPE when opening notification for course not available locally
Partially closes #304

app/src/main/java/crux/bphc/cms/activities/CourseDetailActivity.kt
@@ -21,7 +21,7 @@ import io.realm.Realm
 class CourseDetailActivity : AppCompatActivity() {
     private lateinit var realm: Realm
 
-    private var course: Course? = null
+    private lateinit var course: Course
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,17 +52,17 @@ class CourseDetailActivity : AppCompatActivity() {
             courseId = enrolCourse!!.id
         }
 
-        course = realm
-                .where(Course::class.java)
-                .equalTo("id", courseId)
-                .findFirst()
+        this.course = realm
+            .where(Course::class.java)
+            .equalTo("id", courseId)
+            .findFirst() ?: Course(courseId)
 
         // check if enrolled
-        if (course == null) {
-            setCourseEnrol(enrolCourse!!)
-            title = enrolCourse!!.shortName
+        if (courseId == -1 && enrolCourse != null) {
+            setCourseEnrol(enrolCourse)
+            title = enrolCourse.shortName
         } else {
-            title = course!!.shortName
+            title = course.shortName
 
             val url = Uri.parse(contextUrl) ?: Uri.EMPTY
             if (Urls.isCourseSectionUrl(url) || Urls.isCourseModuleUrl(url)) {
@@ -96,7 +96,7 @@ class CourseDetailActivity : AppCompatActivity() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val courseSectionFragment = CourseContentFragment.newInstance(
                 UserAccount.token,
-                course!!.id,
+                course.id,
                 contextUrl,
         )
         fragmentTransaction.replace(
@@ -111,7 +111,7 @@ class CourseDetailActivity : AppCompatActivity() {
         setCourseContentFragment("")
         supportFragmentManager.executePendingTransactions()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        val forumFragment: Fragment = ForumFragment.newInstance(course!!.id, forumId, course!!.shortName)
+        val forumFragment: Fragment = ForumFragment.newInstance(course.id, forumId, course.shortName)
         fragmentTransaction.addToBackStack(null)
                 .replace(R.id.course_section_enrol_container, forumFragment, "Announcements")
         fragmentTransaction.commit()
@@ -125,10 +125,10 @@ class CourseDetailActivity : AppCompatActivity() {
         supportFragmentManager.executePendingTransactions()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val discussionFragment: Fragment = DiscussionFragment.newInstance(
-            course!!.id,
+            course.id,
             forumId,
             discussionId,
-            course!!.shortName
+            course.shortName
         )
         fragmentTransaction.addToBackStack(null)
                 .replace(R.id.course_section_enrol_container, discussionFragment, "Discussion")

commit 7236614669b8c5ed06bfad79e05b0a46eb0a5f26

Fix: App crash when attempting to enrol to course
Regression from b4a7710d49f684ae2d0fe44d0a52c4e98dbae469.

Close #311

app/src/main/java/crux/bphc/cms/activities/CourseDetailActivity.kt
@@ -3,6 +3,7 @@ package crux.bphc.cms.activities
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
@@ -46,38 +47,43 @@ class CourseDetailActivity : AppCompatActivity() {
         val enrolCourse = intent.getParcelableExtra(INTENT_ENROL_COURSE_KEY) as SearchedCourseDetail?
 
         if (courseId == -1 && enrolCourse == null) {
+            Toast.makeText(this, getString(R.string.invalid_course_id), Toast.LENGTH_SHORT).show();
             finish()
             return
-        } else if (courseId == -1) {
+        }
+
+        if (courseId == -1) {
             courseId = enrolCourse!!.id
         }
 
-        this.course = realm
+        var localCourse = realm
             .where(Course::class.java)
             .equalTo("id", courseId)
-            .findFirst() ?: Course(courseId)
+            .findFirst()
 
-        // check if enrolled
-        if (courseId == -1 && enrolCourse != null) {
+        if (enrolCourse != null && localCourse == null) {
+            // Not available locally, meaning most likely not enroled
             setCourseEnrol(enrolCourse)
             title = enrolCourse.shortName
-        } else {
-            title = course.shortName
+            return
+        }
 
-            val url = Uri.parse(contextUrl) ?: Uri.EMPTY
-            if (Urls.isCourseSectionUrl(url) || Urls.isCourseModuleUrl(url)) {
-                setCourseContentFragment(contextUrl)
-            } else if (Urls.isForumDiscussionUrl(url)){
-                // We parse the json here
-                val customData = Gson().fromJson(customDataStr, NewPostCustomData::class.java)
-                customData.apply {
-                    if (forumId != -1 && discussionId != -1) {
-                        setDiscussionFragment(forumId, discussionId)
-                    }
+        this.course = localCourse ?: Course(courseId)
+        title = course.shortName
+
+        val url = Uri.parse(contextUrl) ?: Uri.EMPTY
+        if (Urls.isCourseSectionUrl(url) || Urls.isCourseModuleUrl(url)) {
+            setCourseContentFragment(contextUrl)
+        } else if (Urls.isForumDiscussionUrl(url)){
+            // We parse the json here
+            val customData = Gson().fromJson(customDataStr, NewPostCustomData::class.java)
+            customData.apply {
+                if (forumId != -1 && discussionId != -1) {
+                    setDiscussionFragment(forumId, discussionId)
                 }
-            } else {
-                setCourseContentFragment("")
             }
+        } else {
+            setCourseContentFragment("")
         }
     }
 

app/src/main/res/values/strings.xml
@@ -92,4 +92,5 @@
     <string name="remove_from_favorites">Remove from favorites</string>
     <string name="addtoFavorites">Add to favorites</string>
     <string name="downloading_file">\"Downloading file - \"</string>
+    <string name="invalid_course_id">Invalid course id!</string>
 </resources>