3858ec9705eb0ef9ceeb52daa79966c822394969
aryanarora180 <aryan.arora180@gmail.com>
2021-06-13 13:40:06 +0530

    Shifted TokenActivity logic to ViewModel to use Coroutines and LiveData

---
+++ app/src/main/java/crux/bphc/cms/models/SingleLiveEvent.kt
@@ -0,0 +1,14 @@
+package crux.bphc.cms.models
+
+class SingleLiveEvent<T>(private val content: T) {
+    private var hasBeenHandled = false
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
