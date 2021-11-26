4a21b96f24dffc9b534faded2ab58af8bffd8559
plaidbait91 <75108809+plaidbait91@users.noreply.github.com>
2021-11-26 14:08:26 +0530

    Fix blank screen on no announcements. (#339)
    Fix blank screen on no announcements.

--- app/src/main/java/crux/bphc/cms/fragments/ForumFragment.kt
+++ app/src/main/java/crux/bphc/cms/fragments/ForumFragment.kt
@@ -113,6 +113,15 @@ class ForumFragment : Fragment() {
                 realm.close()
 
                 CoroutineScope(Dispatchers.Main).launch {
+
+                    if (discussions.size == 0) {
+                        emptyView.text = if (forumId != -1) getString(R.string.no_announcements) else getString(R.string.no_posts_to_display)
+                        emptyView.visibility = View.VISIBLE
+                    } else {
+                        mAdapter.clearDiscussions()
+                        mAdapter.addDiscussions(discussions)
+                    }
+
                     mAdapter.clearDiscussions()
                     mAdapter.addDiscussions(discussions)
                     swipeRefresh.isRefreshing = false
@@ -121,7 +130,9 @@ class ForumFragment : Fragment() {
                 val realmDiscussions = courseDataHandler.getForumDiscussions(forumId)
                 val discussions = realm.copyFromRealm(realmDiscussions)
                 CoroutineScope(Dispatchers.Main).launch {
+
                     if (discussions.size == 0) {
+                        emptyView.text = if (forumId != -1) getString(R.string.no_announcements) else getString(R.string.no_posts_to_display)
                         emptyView.visibility = View.VISIBLE
                         Toast
                             .makeText(context, getString(R.string.no_cached_data), Toast.LENGTH_SHORT)
