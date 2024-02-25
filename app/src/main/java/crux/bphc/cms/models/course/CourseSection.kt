d5129904fd80b4f7d9749bb9de9f10b494f5ecaf
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-02-25 21:56:55 +0530

    Store HTML text in models to be used in HTML Text Field
    Also included is regex to append token to every URL present in the HTML text, as CMS requires for the token to be present as it starts downloading the resource from the URL.

--- app/src/main/java/crux/bphc/cms/models/course/CourseSection.kt
+++ app/src/main/java/crux/bphc/cms/models/course/CourseSection.kt
@@ -3,9 +3,12 @@ package crux.bphc.cms.models.course
 import androidx.core.text.HtmlCompat
 import com.google.gson.annotations.SerializedName
 import crux.bphc.cms.interfaces.CourseContent
+import crux.bphc.cms.models.UserAccount
 import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 /**
  * @author Harshit Agarwal (17-Dec-2016)
@@ -15,7 +18,7 @@ open class CourseSection(
         @PrimaryKey  @SerializedName("id") var id: Int = 0,
         name: String = "",
         @SerializedName("section") var sectionNum: Int = 0,
-        @SerializedName("summary") var summary: String = "",
+        summary: String = "",
         @SerializedName("modules") var modules: RealmList<Module> = RealmList<Module>(),
         var courseId: Int = 0,
 ) : RealmObject(), CourseContent {
@@ -23,16 +26,33 @@ open class CourseSection(
     @SerializedName("name") var name: String = name
         get() {
             return HtmlCompat.fromHtml(field, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
-                    .trim { it <= ' ' }
+                .trim { it <= ' ' }
+        }
+
+    @SerializedName("summary") var summary: String = summary
+        get() {
+            val pattern: Pattern = Pattern.compile(URL_REGEX)
+            val matcher: Matcher = pattern.matcher(field)
+
+            val summaryBuffer = StringBuffer(field.length)
+
+            while (matcher.find()) {
+                val foundLink = matcher.group(1)
+                val replaceWith = "<a href=\"$foundLink?token=${UserAccount.token}\">"
+                matcher.appendReplacement(summaryBuffer, replaceWith)
+            }
+            matcher.appendTail(summaryBuffer)
+
+            return summaryBuffer.toString().trim { it <= ' ' }
         }
 
     fun deepCopy(): CourseSection  = CourseSection(
-            id,
-            name,
-            sectionNum,
-            summary,
-            RealmList<Module>(*modules.map { it.deepCopy() }.toTypedArray()),
-            courseId,
+        id,
+        name,
+        sectionNum,
+        summary,
+        RealmList<Module>(*modules.map { it.deepCopy() }.toTypedArray()),
+        courseId,
     )
 
     override fun equals(other: Any?): Boolean {
@@ -41,5 +61,9 @@ open class CourseSection(
 
     override fun hashCode(): Int {
         return id
+    }
+
+    companion object {
+        private const val URL_REGEX = "<a href=\"(.*?)\">"
     }
 }
